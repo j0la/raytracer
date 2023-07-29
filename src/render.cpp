3ed184c10683767a8d37f47bf4bec5c0aa151426
Jonathan Olaleye <69396515+j0la@users.noreply.github.com>
2023-07-28 23:32:27 -0500

    added transparency rays

--- src/render.cpp
+++ src/render.cpp
@@ -19,12 +19,12 @@ void render(Scene& scene, std::vector<std::vector<Color>>& pixels) {
                 .origin = scene.eye_pos,
                 .dir = v_norm(pixel_center - scene.eye_pos)
             };
-            pixels[i][j] = cast_ray(scene, ray, 1);
+            pixels[i][j] = cast_ray(scene, ray, 1, 1);
         }
     }
 }
 
-Color cast_ray(Scene& scene, Ray& ray, int depth) {
+Color cast_ray(Scene& scene, Ray& ray, float ni, int depth) {
     // intersection displacement along ray
     float t = -1;
     // intersected sphere index
@@ -48,11 +48,12 @@ Color cast_ray(Scene& scene, Ray& ray, int depth) {
     Sphere& sphere = scene.spheres.at(s);
     Material& material = scene.materials.at(sphere.m);
 
-    // surface normal at intersection
-    Vector N = v_norm((ipt - sphere.origin) / sphere.radius);
-
     // direction to eye / ray origin
     Vector V = v_norm(ray.dir * -1);
+
+    // surface normal at intersection
+    Vector N = v_norm(ipt - sphere.origin);
+    if (N * V < 0) N = N * -1; // use normal direction closer to incoming ray
 
     Color energy = material.od * material.ka;
 
@@ -65,13 +66,13 @@ Color cast_ray(Scene& scene, Ray& ray, int depth) {
         Vector H = v_norm(L + V);
 
         // spawn shadow ray
-        int shadow = 1;
+        float shadow = 1;
         Ray shadow_ray = { .origin = ipt, .dir = L };
         for (Sphere& sphere : scene.spheres) {
             float t = intersect(shadow_ray, sphere);
             // check for intersection between surface & light
             if (t > MIN_IXN_DISTANCE && t < light_d) {
-                shadow = 0;
+                shadow = shadow * (1 - scene.materials.at(sphere.m).a);
             }
         }
 
@@ -83,18 +84,39 @@ Color cast_ray(Scene& scene, Ray& ray, int depth) {
 
     if (depth >= MAX_RAY_DEPTH) return energy;
 
+    float cos_i = V * N;
+
+    // material fresnel reflectance
+    float f0 = std::pow((material.n - ni) / (material.n + ni), 2);
+    float fr = f0 + (1 - f0) * std::pow(1 - cos_i, 5);
+
     // cast reflection ray
     if (material.ks > 0) {
-        // material fresnel reflectance
-        float f0 = std::pow((material.n - 1) / (material.n + 1), 2);
-        float fr = f0 + (1 - f0) * std::pow(1 - (V * N), 5);
-
         Ray R = {
             .origin = ipt,
-            .dir = v_norm(N * 2 * (N * V) - V)
+            .dir = v_norm(N * 2 * cos_i - V)
         };
-        energy = energy + cast_ray(scene, R, depth + 1) * fr;
+        energy = energy + cast_ray(scene, R, ni, depth + 1) * fr;
         energy = clamp(energy, 0, 1);
+    }
+
+    // cast transmission ray
+    if (material.a < 1) {
+        float n_ratio = ni / material.n;
+        float cos_t = 1 - std::pow(n_ratio, 2) * (1 - std::pow(cos_i, 2));
+        // total internal reflection when sqrt(cos_t) is undefined (cos_t < 0)
+        if (cos_t >= 0) {
+            cos_t = std::sqrt(cos_t);
+
+            Ray T = {
+                .origin = ipt,
+                .dir = v_norm((N * -1) * cos_t + (N * cos_i - V) * n_ratio)
+            };
+            T.origin = T.origin + T.dir;
+
+            energy = energy + cast_ray(scene, T, material.n, depth + 1) * (1 - fr) * (1 - material.a);
+            energy = clamp(energy, 0, 1);
+        }
     }
 
     return energy;
@@ -110,8 +132,12 @@ void write_ppm(std::vector<std::vector<Color>>& pixels, std::string path) {
 
     fs << "P3 " << pixels.size() << " " << pixels.front().size() << " 255" << std::endl;
 
-    for (std::vector<Color>& col : pixels) {
-        for (Color& pixel : col) {
+    size_t cols = pixels.size();
+    size_t rows = pixels.front().size();
+
+    for (size_t j = 0; j < rows; j++) {
+        for (size_t i = 0; i < cols; i++) {
+            Color pixel = pixels[i][j];
             fs << std::to_string((uint8_t)(pixel.r * 255)) << " "
                 << std::to_string((uint8_t)(pixel.g * 255)) << " "
                 << std::to_string((uint8_t)(pixel.b * 255)) << std::endl;
--- src/render.hpp
+++ src/render.hpp
@@ -5,10 +5,10 @@
 #include "scene.hpp"
 
 #define MIN_IXN_DISTANCE 0.1 // offset to avoid self-intersections
-#define MAX_RAY_DEPTH 10 // reflection recursion depth
+#define MAX_RAY_DEPTH 5 // reflection recursion depth
 
 void render(Scene& scene, std::vector<std::vector<Color>>& pixels);
-Color cast_ray(Scene& scene, Ray& ray, int depth);
+Color cast_ray(Scene& scene, Ray& ray, float ni, int depth);
 void write_ppm(std::vector<std::vector<Color>>& pixels, std::string path);
 
 #endif
